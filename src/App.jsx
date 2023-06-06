22f692c483e72394ecc81894c810e7a9468c3aa6
randyChrisp <79rchrisp@gmail.om>
2023-06-05 21:18:11 -0400

    Added layout and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About.jsx";
 // import Nav from "./components/Nav";
-import Header from "./components/Header";
+import Layout from "./components/Layout";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail";
 import "./server";
@@ -12,12 +12,13 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header></Header>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
+import { Outlet } from "react-router-dom";
 
 const Header = () => {
   return (
