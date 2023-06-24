891a9a83f3e6f7cffcc17039230208145d8894df
randyChrisp <79rchrisp@gmail.om>
2023-06-23 21:59:16 -0400

    Added avatar icon in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import imageUrl from "../images/avatar-icon.png";
 
 const Header = () => {
   const activeStyles = {
@@ -28,6 +29,9 @@ const Header = () => {
           style={({ isActive }) => (isActive ? activeStyles : null)}>
           Vans
         </NavLink>
+        <Link to="login" className="login-link">
+          <img src={imageUrl} className="login-icon" />
+        </Link>
       </nav>
     </header>
   );
